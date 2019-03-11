55dadb0eb5fd365ee2a7a3956b7425b2d7ff400c
Paul Brodner <paulbrodner@gmail.com>
2019-03-11 18:03:32 +0200

    initializing jenkins build infrastructure on AWS

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# build-infrastructure
-Build Infrastrcture
+# Build Infrastructure
+### Prerequisites
+* Terraform [installed](https://learn.hashicorp.com/terraform/getting-started/install) on your machine
+* AWS Credentials in [place](https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/credentials.html)
+* EC2 Key Pairs for accessing machine(s)
+* AWS [IAM Roles](https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles.html) with permissions for resources that we will create using [main.tf](./main.tf).
+
+### Preparation
+> this is done once!
+
+a) clone the repository
+```shell
+$ git clone https://github.com/awesome-accelerators/build-infrastructure
+```
+
+b) create a new Amazon Key Pairs
+> see [guide](https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-key-pairs.html#having-ec2-create-your-key-pair)
+> copy to ./secrets folder
+> update [variables.tf](./variables.tf) to point to this key
+
+c) Initialize the Terraform working directory
+```shell
+$ terraform init
+```
+
+d) Generate and show the execution plan
+```shell
+$ terraform plan
+```
+> You should see now the execution plan of Terraform with all services/resources that will be created. 
+
+### Building
+a) starting the build Infrastructure up
+```shell
+$ make infra
+```
+
+b) deleting the entire infrastructure
+```shell
+$ make destroy
+```
---
+++ scripts/install-jenkins.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+set -exu
+
+plugins=$1
+
+function wait_jenkins_to_start() {
+    echo "Waiting jenkins to launch on 8080..."
+
+    while ! nc -z localhost 8080; do
+      sleep 0.1 # wait for 1/10 of the second before check again
+    done
+
+    echo "Jenkins launched"
+    sleep 2        
+}
+
+sudo yum update -y
+echo "* Installing java 1.8"
+sudo yum install -y java-1.8.0-openjdk-devel
+
+echo "* Set default java"
+sudo /usr/sbin/alternatives --set java /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java
+sudo yum remove -y java-1.7*
+
+
+echo "* Import GPG key"
+curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+
+echo "* add jenkins repo"
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+
+
+echo "* Install latest Jenkins"
+sudo yum install -y jenkins-2.150.2-1.1.noarch
+
+echo "Tool for handling xml files"
+sudo yum install -y xmlstarlet
+
+echo "* Start Jenkins without asking to install plugins"
+export JAVA_OPTS=-Djenkins.install.runSetupWizard=false
+
+sudo service jenkins start
+sudo chkconfig --add jenkins
+sudo chkconfig jenkins on
+
+wait_jenkins_to_start
+sleep 20
+ADMIN_PASSW=$(sudo bash -c "cat /var/lib/jenkins/secrets/initialAdminPassword")
+
+sudo wget http://localhost:8080/jnlpJars/jenkins-cli.jar -O /var/lib/jenkins/jenkins-cli.jar
+
+echo "* Installing the following plugins: ${plugins}"
+sudo java -jar /var/lib/jenkins/jenkins-cli.jar -s http://localhost:8080 -auth admin:$ADMIN_PASSW install-plugin ${plugins}
+sudo java -jar /var/lib/jenkins/jenkins-cli.jar -s http://localhost:8080 -auth admin:$ADMIN_PASSW restart
+
+echo "Cleanup"
+sudo yum -y clean all
+sudo find /var/log -type f -delete
+sudo find /tmp -type f -delete
+sudo find /root /home -name '.*history' -delete
+wait_jenkins_to_start
